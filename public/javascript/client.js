669506387be432c0432527d8122806f3ac1d0a3f
nikelei <zlooo52033@163.com>
2017-02-03 10:58:11 +0800

    修复部分bug

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -95,9 +95,9 @@ $(document).ready(function() {
     }
     // 匹配表情字符
     function replace_em(str) {
-        // str = str.replace(/\</g, '&lt;');
-        // str = str.replace(/\>/g, '&gt;');
-        // str = str.replace(/\n/g, '<br/>');
+        str = str.replace(/\</g, '&lt;');
+        str = str.replace(/\>/g, '&gt;');
+        str = str.replace(/\n/g, '<br/>');
         str = str.replace(/\[em_([0-9]*)\]/g, '<img src="images/face/$1.gif" border="0" />');
         return str;
     }
